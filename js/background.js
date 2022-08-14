25d2393f59756f4a87ff0b06ffb40f790ce8dcd1
Hwanie91 <jh27hot@gmail.com>
2022-08-14 22:07:30 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,7 +1,10 @@
 const images = ["a1.jpg", "a2.jpg", "a3.jpg", "a4.jpg", "a5.jpg", "a6.jpg", "a7.jpg", "a8.jpg", "a9.jpg"];
+const BGI_CLASSNAME_KEY = "bgi";
 
 const chosenImage =images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.classList.add(BGI_CLASSNAME_KEY);
 document.body.appendChild(bgImage);
+
